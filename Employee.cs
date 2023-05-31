c238849a705e970dd5e49ef88b34ec2e323bc78a
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-05-31 13:56:47 -0700

    create Employee getters

--- Employee.cs
+++ Employee.cs
@@ -2,22 +2,32 @@ namespace CatWorx.BadgeMaker
 {
     class Employee
     {
-        public string FirstName;
-        public string LastName;
-        public int Id;
-        public string PhotoUrl;
+        private string FirstName;
+        private string LastName;
+        private int Id;
+        private string PhotoUrl;
 
-        public Employee(string firstName, string lastName) //, int id, string photoUrl) 
+        public Employee(string firstName, string lastName, int id, string photoUrl) 
         {
             FirstName = firstName;
             LastName = lastName;
-            // Id = id;
-            // PhotoUrl = photoUrl;
+            Id = id;
+            PhotoUrl = photoUrl;
         }
 
         public string GetFullName()
         {
             return FirstName + " " + LastName;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public string GetPhotoUrl()
+        {
+            return PhotoUrl;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -18,17 +18,22 @@ namespace CatWorx.BadgeMaker
             // Collect user values until the value is an emty string
             while (true)
             {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine() ?? "";
-                // Break if the user hits ENTER without typing a name
-                if (input == "")
+                // Move the initial prompt inside the loop, so it repeats for each employee
+                Console.WriteLine("Enter first name (leave empty to exit): ");
+                // change input to firstName
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
                 {
                     break;
                 }
-
-                // Create a new Employee instance
-
-                Employee currentEmployee = new Employee(input, "Smith");
+                // add a Console.ReadLine() for each value
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
                 employees.Add(currentEmployee);
             }
 
@@ -39,7 +44,8 @@ namespace CatWorx.BadgeMaker
         {
             for (int i = 0; i < employees.Count; i++)
             {
-                Console.WriteLine(employees[i].GetFullName());
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
             }
         }
     }
