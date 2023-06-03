e3f430a58f3984112cadb7de233ed3a1cdc60b0a
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-06-02 17:25:23 -0700

    add logic to allow user to import or enter employees

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace CatWorx.BadgeMaker
 {
@@ -42,6 +43,26 @@ namespace CatWorx.BadgeMaker
             using (HttpClient client = new HttpClient())
             {
                 string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+                JObject json = JObject.Parse(response);
+                // JArray results = (JArray)json["results"];
+
+                // foreach (JObject result in results)
+                // {
+                //     Console.WriteLine(result.SelectToken("name.first"));
+                // }
+
+                foreach (var token in json.SelectToken("results")!)
+                {
+                    // Parse JSON data
+                    Employee emp = new Employee
+                    (
+                      token.SelectToken("name.first")!.ToString(),
+                      token.SelectToken("name.last")!.ToString(),
+                      Int32.Parse(token.SelectToken("id.value")!.ToString().Replace("-", "")),
+                      token.SelectToken("picture.large")!.ToString()
+                    );
+                    employees.Add(emp);
+                }
             }
             return employees;
         }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,33 @@ namespace CatWorx.BadgeMaker
     {
         async static Task Main(string[] args)
         {
-            // List<Employee> employees = PeopleFetcher.GetEmployees();
-            List<Employee> employees = await PeopleFetcher.GetFromApi();
+            List<Employee> employees = new List<Employee>();
+            string selection = "";
+            Console.WriteLine($"How do you wish to add employee data? Enter \"1\" or \"2\" below to make your selection.");
+
+            while (selection != "1" && selection != "2")
+            {
+                if (selection != "")
+                {
+                    Console.WriteLine("Invalid selection. Enter \"1\" or \"2\" below to make your selection.");
+                }
+                Console.WriteLine("1. Automatically import employees");
+                Console.WriteLine("2. Add employees manually");
+                selection = Console.ReadLine() ?? "";
+            }
+
+            switch (selection)
+            {
+                case "1":
+                    employees = await PeopleFetcher.GetFromApi();
+                    break;
+                case "2":
+                    employees = PeopleFetcher.GetEmployees();
+                    break;
+                default:
+                    Console.WriteLine("Oops! Something went wrong");
+                    break;
+            }
 
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
